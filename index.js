60cff288cf234d6f956314f8c83d23c38b1543bd
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-12 22:53:21 -0300

    adding chalk

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import cats from "cat-ascii-faces";
+import chalk from "chalk";
 
 export default function coolsolelog(msg) {
-    return (`  ${cats()} Olá humano, você tem uma miausagem nova!
-    ^-----------------------------------------^
-    |                                         |
-    |                                         |
-    |            o               o            |
+    return chalk.magenta.bgYellow(`  ${cats()} Olá humano, você tem uma miausagem nova!
+    ^-----------------------------------------^ 
+    |                                         | 
+    |                                         | 
+    |            o               o            | 
    <        =           w            =         >         
-    |                                         |         
-    [v]                                     [v]
-    ----  ${msg}                            
+    |                                         |          
+    [v]                                     [v] 
+    ----  ${msg}                                                           
     `);
 }
